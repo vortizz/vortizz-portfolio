f4fd50acc743c5532428f4c81c0d57e84b7f2a6b
Victor Ortiz <ortizvictor154@gmail.com>
2026-04-01 17:36:00 +1100

    fix: prerender project pages to resolve SSR content query failure on Vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ['/projects'],
+    },
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/content'],
   colorMode: {
